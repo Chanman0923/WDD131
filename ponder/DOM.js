7dd2ed955fb1bd165e6032b10180aa5a5f35c18b
Chanman0923 <pin21021@byui.edu>
2026-09-25 18:53:44 -0600

    update

---
+++ ponder/DOM.js
@@ -0,0 +1,8 @@
+const title = document.querySelector('h1');
+console.log(title);
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
